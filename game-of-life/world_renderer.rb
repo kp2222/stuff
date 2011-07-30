7844e02edeae01c02889f813e5a94fe179875330
krishnaprasad <kp2222@gmail.com>
2011-07-31 00:47:53 +0530

    Game of life a simple implementation

---
+++ game-of-life/world_renderer.rb
@@ -0,0 +1,31 @@
+class WorldRenderer
+
+  attr_accessor :world
+
+  def initialize(world)
+    @world =  world
+  end
+
+  def render
+    system("clear")
+    @world.map.each do |row|
+      row.each do |cell|
+          print green "." if cell == 0
+          print  red "*"  if cell == 1
+      end
+      print "\t"
+    end
+    print "\n"
+  end
+  
+  private
+  
+  def colorize(text, color_code)
+    "\e[#{color_code}m#{text}\e[0m"
+  end
+  
+  def red(text); colorize(text, 31); end
+  
+  def green(text); colorize(text, 32); end
+  
+end
