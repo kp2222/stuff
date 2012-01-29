0a8d0f5f46e16a694bca08e90cbb0961e8144a28
krishnaprasad <kp2222@gmail.com>
2012-01-29 13:37:40 +0530

    use colorize gem for console output

--- game-of-life/world_renderer.rb
+++ game-of-life/world_renderer.rb
@@ -1,31 +1,27 @@
+require 'colorize'
+
 class WorldRenderer
 
   attr_accessor :world
+  
 
   def initialize(world)
     @world =  world
+    @live_cell =   "*".red
+    @dead_cell = ".".black
   end
 
   def render
     system("clear")
     @world.map.each do |row|
       row.each do |cell|
-        print green "." if cell == 0
-        print  red "*"  if cell == 1
+        print @dead_cell if cell == 0
+        print @live_cell  if cell == 1
         print " "
       end
       print "\n"  
     end
   end
   
-  private
-  
-  def colorize(text, color_code)
-    "\e[#{color_code}m#{text}\e[0m"
-  end
-  
-  def red(text); colorize(text, 31); end
-  
-  def green(text); colorize(text, 32); end
-  
+
 end
